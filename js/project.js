0daf332f9268c35dd70eece2904936994e6ea6c5
ceijoan <166540233+ceijoan@users.noreply.github.com>
2024-12-28 18:46:38 +0100

    nuevos ajustes

--- js/project.js
+++ js/project.js
@@ -67,11 +67,60 @@ document.addEventListener("DOMContentLoaded", function (){
     //Eventos de accion de los botones
     btnAnteriorProject.addEventListener("click", prevProyect);
     btnSiguienteProject.addEventListener("click", nextProyect);
+    const servicios = [
+    { id: 1, titulo: 'Tu marca, tu identidad.', descripcion: 'Entendemos que la importancia de tu marca va mas alla de un logo y sus colores. Te ayudamos a encontrar el balance adecuado entre imágen y comunicación para crear una conexión con tus usuarios que resalte.', titulodebarra: 'Estrategia de Marca', imagen: 'No tiene cobertura' },
+    { id: 2, titulo: 'titulo 2', descripcion: 'descripcion 2', titulodebarra: 'Estrategia de Marca 2', imagen: 'No tiene cobertura' },
+    { id: 3, titulo: 'titulo3', descripcion: 'descripcion 3', titulodebarra: 'Estrategia de Marca 3', imagen: 'No tiene cobertura' },
+    { id: 4, titulo: 'titulo4', descripcion: 'descripcion 4', titulodebarra: 'Estrategia de Marca 4', imagen: 'No tiene cobertura' },
+    { id: 5, titulo: 'titulo5', descripcion: 'descripcion 5', titulodebarra: 'Estrategia de Marca 5', imagen: 'No tiene cobertura' }
+    ];
+    
+    //Crear el slider con JS
+    const serviciosContainer = document.getElementById('Slider-ServiciosBox');
+    servicios.forEach((servicio, index) => {
+        const div = document.createElement('div');
+        if (servicios.length = 4) {
+            div.innerHTML = 
+                `<div class="Slider-box">
+                    <span class="Slider-img"><p class="Slider-txtContentBox u-colorBlanco">${servicio.titulodebarra}</p></span>
+                </div>`
+            ;
+        };
+        serviciosContainer.appendChild(div);
+    });
+    
+    function renderSlider() {
+        const serviciosContainer2 = document.getElementById('Slider-ServiciosBox');
+        serviciosContainer.innerHTML = ''; 
+        servicios.slice(0, 5).forEach(servicio => {
+            const div = document.createElement('div');
+            div.classList.add('Slider-box');
+            div.innerHTML = 
+                `<span class="Slider-img">
+                    <p class="Slider-txtContentBox u-colorBlanco">${servicio.titulodebarra}</p>
+                </span>`
+            ;
+            serviciosContainer2.appendChild(div);
+        });
+    } 
+
+    function nextSlide() {
+        const firstService = servicios.shift();
+        servicios.push(firstService);
+        console.log(firstService);
+        renderSlider();
+        
+    }
+    
+    renderSlider();
+    window.nextSlide = nextSlide;
+    
+    
+    
     //variable que inicializa el estado de los elementos
     let indice2 = 0;
-    const totalProyectos = divProyecto.length;
-
-
+    const totalProyectos = servicios.length;
+    
     function nextProyect(){
         indice2++;
         console.log(totalProyectos);
